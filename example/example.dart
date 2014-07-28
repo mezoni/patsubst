011ccb9d25339e74d288f7583f5f932bdfe73d07
unknown <D:\MAIL>
2014-07-28 20:39:25 +0600

    0.0.9

--- example/example.dart
+++ example/example.dart
@@ -1,21 +1,9 @@
-patsubst
-=====
-
-Pattern based text substitution in strings.
-
-Examples:
-
-```dart
 import "package:patsubst/patsubst.dart";
 
 void main() {
   var subst = patsubst("foo/%/%1.cc", "wow/%/with/kara_%2.obj");
   var source = "foo/bar/baz1.cc";
   var result = subst.replace(source);
-  print("$source => $result");  
+  print("$source => $result");
+  // foo/bar/baz1.cc => wow/bar/with/kara_baz2.obj
 }
-```
-
-Output:
-
-```foo/bar/baz1.cc => wow/bar/with/kara_baz2.obj```
